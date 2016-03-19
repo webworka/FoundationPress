4b168e8a6f061e391f3e0866d53283482b2dfd62
oli <oliver.albrecht.berlin@gmail.com>
2016-03-19 15:56:25 +0100

    - removed sticky footer - removed all js and sass includes

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ var COMPATIBILITY = ['last 2 versions', 'ie >= 9'];
 var PATHS = {
   sass: [
     'assets/components/foundation-sites/scss',
-    'assets/components/motion-ui/src',
+    //'assets/components/motion-ui/src',
     'assets/components/fontawesome/scss',
   ],
   javascript: [
@@ -35,28 +35,28 @@ var PATHS = {
     'assets/components/foundation-sites/js/foundation.util.*.js',
 
     // Paths to individual JS components defined below
-    'assets/components/foundation-sites/js/foundation.abide.js',
-    'assets/components/foundation-sites/js/foundation.accordion.js',
-    'assets/components/foundation-sites/js/foundation.accordionMenu.js',
-    'assets/components/foundation-sites/js/foundation.drilldown.js',
-    'assets/components/foundation-sites/js/foundation.dropdown.js',
-    'assets/components/foundation-sites/js/foundation.dropdownMenu.js',
-    'assets/components/foundation-sites/js/foundation.equalizer.js',
-    'assets/components/foundation-sites/js/foundation.interchange.js',
-    'assets/components/foundation-sites/js/foundation.magellan.js',
-    'assets/components/foundation-sites/js/foundation.offcanvas.js',
-    'assets/components/foundation-sites/js/foundation.orbit.js',
-    'assets/components/foundation-sites/js/foundation.responsiveMenu.js',
-    'assets/components/foundation-sites/js/foundation.responsiveToggle.js',
-    'assets/components/foundation-sites/js/foundation.reveal.js',
-    'assets/components/foundation-sites/js/foundation.slider.js',
-    'assets/components/foundation-sites/js/foundation.sticky.js',
-    'assets/components/foundation-sites/js/foundation.tabs.js',
-    'assets/components/foundation-sites/js/foundation.toggler.js',
-    'assets/components/foundation-sites/js/foundation.tooltip.js',
+    //'assets/components/foundation-sites/js/foundation.abide.js',
+    //'assets/components/foundation-sites/js/foundation.accordion.js',
+    //'assets/components/foundation-sites/js/foundation.accordionMenu.js',
+    //'assets/components/foundation-sites/js/foundation.drilldown.js',
+    //'assets/components/foundation-sites/js/foundation.dropdown.js',
+    //'assets/components/foundation-sites/js/foundation.dropdownMenu.js',
+    //'assets/components/foundation-sites/js/foundation.equalizer.js',
+    //'assets/components/foundation-sites/js/foundation.interchange.js',
+    //'assets/components/foundation-sites/js/foundation.magellan.js',
+    //'assets/components/foundation-sites/js/foundation.offcanvas.js',
+    //'assets/components/foundation-sites/js/foundation.orbit.js',
+    //'assets/components/foundation-sites/js/foundation.responsiveMenu.js',
+    //'assets/components/foundation-sites/js/foundation.responsiveToggle.js',
+    //'assets/components/foundation-sites/js/foundation.reveal.js',
+    //'assets/components/foundation-sites/js/foundation.slider.js',
+    //'assets/components/foundation-sites/js/foundation.sticky.js',
+    //'assets/components/foundation-sites/js/foundation.tabs.js',
+    //'assets/components/foundation-sites/js/foundation.toggler.js',
+    //'assets/components/foundation-sites/js/foundation.tooltip.js',
 
     // Motion UI
-    'assets/components/motion-ui/motion-ui.js',
+    //'assets/components/motion-ui/motion-ui.js',
 
     // Include your own custom scripts (located in the custom folder)
     'assets/javascript/custom/*.js',
